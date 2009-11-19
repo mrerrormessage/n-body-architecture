6f1930a0d8a43f61f2931f9284929b551619d433
Robert Grider <mrerrormessage@gmail.com>
2009-11-18 20:14:39 -0600

    added test specifications and updated serial.c

--- serial/serial.c
+++ serial/serial.c
@@ -1,15 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 #define GRAV_CONST 1.000
 #define TIME_STEP 0.5
 #define NUM_BODIES 50
 #define WORLD_X_SIZE 500.0
 #define WORLD_Y_SIZE 500.0
+#define GRAV_CONSTANT 6.67428e-11
 
 struct body {
   float x_posn;
   float y_posn;
+  float x_velocity;
+  float y_velocity;
   float mass;
 };
 
@@ -78,6 +82,8 @@ struct cosmos * get_cosmos( int n){
     b->x_posn = 2.0;
     b->y_posn = 2.0;
     b->mass = 1.0;
+    b->x_velocity = 0.0;
+    b->y_velocity = 0.0;
   }
    
   return c;
@@ -105,6 +111,79 @@ void print_cosmos( struct cosmos * c ){
   return;
 
 }
+
+float sign( float f ){
+  if (f == 0.0){
+    return 0.0;
+  }
+  if (f < 0.0){
+    return -1.0;
+  }
+  return 1.0;
+}
+
+void calc_gravity (struct cosmos * c, struct body * b){
+  float xdiff;
+  float ydiff;
+  float dist_sq;
+  float force;
+  float dist;
+  float theta;
+  //  float x_accel = 0.0;
+  //  float y_accel = 0.0;
+  float x_force_sum = 0.0;
+  float y_force_sum = 0.0;
+  for(int i = 0; i < c->num_bodies; i++){
+    struct body * b2;
+    b2 = get_body(c, i);
+
+    if( b2 != b ){
+      xdiff = b->x_posn - b2->x_posn;
+      ydiff = b->y_posn - b2->y_posn;
+      //note that this is actually distance squared, but that's the term we need anyhow
+      dist_sq = pow( xdiff, 2.0) + pow(ydiff, 2.0);
+      force = GRAV_CONST * (b->mass) * (b2->mass) / dist_sq;
+      //now we need to split it out into two separate vectors
+      dist = sqrt(dist_sq);
+      theta = atan(ydiff/xdiff);
+      x_force_sum += force * sign(xdiff) * cos(theta);
+      y_force_sum += force * sign(ydiff) * sin(theta);
+    }
+  }
+  
+  b->x_velocity += (x_force_sum / b->mass);
+  b->y_velocity += (y_force_sum / b->mass);
+  
+  //may need to have this divided by time_step, not sure
+  b->x_posn += b->x_velocity * c->time_step;
+  b->y_posn += b->y_velocity * c->time_step;
+
+  
+  return;
+}
+
+//one iteration of the n_body
+void simple_n_body_iter ( struct cosmos * c ){
+
+  for(int i = 0; i < c->num_bodies; i++){
+    struct body * b; 
+    b = get_body( c, i ); 
+    calc_gravity( c, b );
+  }
+  
+  return;
+}
+
+//n-body simple. 
+void simple_n_body ( struct cosmos * c, int steps ){
+
+  for( int i = 0; i < steps ; i++ ){
+    simple_n_body_iter ( c );
+  }
+
+  return;
+}
+
 
 int main(){
 
