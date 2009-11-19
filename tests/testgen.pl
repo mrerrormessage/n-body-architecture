9a3dfe4c4cf2ee93f8326cc64ffaf7ab5f02fc69
Robert Grider <mrerrormessage@gmail.com>
2009-11-19 01:18:03 -0600

    added early version of test-case generator script

---
+++ tests/testgen.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+#the name of the file should be taken as a command line argument
+open( my $out, ">", );
+
+#first line defines world size, time-step, and number of objects
+print("\[ ...., ...., ...., ...\]");
+
+for (my $i = 0; $i < ... ; $i++){
+
+#command-line argument
+    my $mass = random(  );
+
+#command-line argument
+    my $x = random( );
+    
+#command-line argument
+    my $y = random( );
+    
+#each line is printed here
+    printf("%f %f %f\n", mass, x, y);
+    
+}
+
+#close the file here
