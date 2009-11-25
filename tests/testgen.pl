c59778f251ef3025dce5fadf1da4d0f74873de84
Robert Grider <mrerrormessage@gmail.com>
2009-11-24 22:52:16 -0600

    reworked test generation

--- tests/testgen.pl
+++ tests/testgen.pl
@@ -11,7 +11,7 @@ if($ARGV[0] =~ m/--help/){
 }
 
 #first line defines world size, time-step, and number of objects
-print("\[ $ARGV[0], $ARGV[1], $ARGV[2], $ARGV[3]\]\n");
+print("\:$ARGV[0] $ARGV[1] $ARGV[2] $ARGV[3]\n");
 
 for (my $i = 0; $i < $ARGV[3] ; $i++){
 
