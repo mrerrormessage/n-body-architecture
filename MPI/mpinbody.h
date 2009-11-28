f15753b734586cf194718254a8e269ab9a2424ef
Robert Grider <mrerrormessage@gmail.com>
2009-11-28 13:06:06 -0500

    adding MPI base files to git

---
+++ MPI/mpinbody.h
@@ -0,0 +1,16 @@
+#ifndef _MPINBODY_H_
+#define _MPINBODY_H_
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include "cosmos.h"
+#include "nbodyutil.h"
+
+#define GRAV_CONST 1.000
+#define TIME_STEP 0.5
+#define GRAV_CONSTANT 6.67428e-11
+
+
+
+#endif
