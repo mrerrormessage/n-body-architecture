348b7e2a613685fba728a4065ec3e5dee0b23d82
Robert Grider <mrerrormessage@gmail.com>
2009-11-28 12:53:39 -0500

    Seperated libraries out from the body of the serial code. Everything is now much cleaner and able to be moved nicely to the parallel implementations

---
+++ serial/nbodyutil.c
@@ -0,0 +1,75 @@
+#include "nbodyutil.h"
+
+//converts a string to a floating point number
+float string_to_float( char * s ){
+  char * nums[2];
+  int resct = chunk_string( s, ".", 2, nums);
+  if(0 == resct || 2 < resct){
+    printf("string: \"%s\" is not a float!\n", s);
+    return 0.0;
+  }
+  if(1 == resct){
+    return (float)atol(nums[0]);
+  }
+  return (float)atol(nums[0]) + (atol(nums[1]) / pow(10, (int)strlen(nums[1])) ) ;
+}
+
+//gets the next good line, checking for EOF and ignoring whitespace and commented lines
+int getnextgoodline( char * s, int n, FILE * f){
+  fgets(s, n, f);
+  if(NULL == s){
+    return 1;
+    //semicolon escapes commented lines
+  }else if(';' == s[0] || '\n' == s[0]){
+    return getnextgoodline( s, n, f);    
+  }
+  return 0;
+}
+
+//splits a string into chunks based on the delimiter, resultc is max results
+//returns number of results retrieved
+int chunk_string(char * s, char * delim, int resultc, char ** res){
+  if(0 == resultc || '\n' == s[0] || '\0' == s[0]){
+    return 0;
+  }
+  int ch_cnt = strcspn( s, delim );
+  res[0] = malloc( (ch_cnt + 1) * sizeof(char));
+  if(NULL == res[0]){
+    printf("error in malloc\n");
+    exit(0);
+  }
+  strncpy(res[0], s, (ch_cnt));
+  if('\0' == s[ch_cnt] || '\n' == s[ch_cnt] ){
+    return 1;
+  } 
+  return 1 + chunk_string( &(s[ch_cnt + 1]), delim, --resultc, &(res[1]));
+}
+
+//frees chunk-based arrays
+void free_chunks(char ** c, int num){
+  
+  for(int i = 0; i < num; i++){
+    free(c[i]);
+  }
+
+  return;
+}
+
+int min( int x, int y ){
+  if (x < y){
+    return x; 
+  }
+  return y;
+}
+
+float sign( float f ){
+  if (f == 0.0){
+    return 0.0;
+  }
+  if (f < 0.0){
+    return -1.0;
+  }
+  return 1.0;
+}
+
+
--- serial/serial.h
+++ serial/serial.h
@@ -4,51 +4,15 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
-
+#include "cosmos.h"
 
 #define GRAV_CONST 1.000
 #define TIME_STEP 0.5
-#define NUM_BODIES 50
-#define WORLD_X_SIZE 500.0
-#define WORLD_Y_SIZE 500.0
+//#define NUM_BODIES 50
+//#define WORLD_X_SIZE 500.0
+//#define WORLD_Y_SIZE 500.0
 #define GRAV_CONSTANT 6.67428e-11
 
-struct body {
-  float x_posn;
-  float y_posn;
-  float x_velocity;
-  float y_velocity;
-  float x_force;
-  float y_force;
-  float mass;
-};
-
-struct cosmos {
-  int num_bodies;
-  float x_world_size;
-  float y_world_size;
-  struct body * body_list;
-  float time_step;
-};
-
-
-//cosmos_init initializes a cosmos with parameters as given
-struct cosmos * cosmos_init( float xsize, float ysize, float ts, int numbodies);
-
-//returns body k in cosmos c
-struct body * get_body(struct cosmos * c, int k);
-
-//cosmos_free frees a cosmos
-void cosmos_free( struct cosmos * c );
-
-//get_bodies fills the cosmos with bodies. How we're getting the bodies is very unclear at this point
-//we may take away the parameter n, which tells how many bodies to fetch
-//this is a dummy test function to give a sample n-body cosmos - a trivial 
-//cosmos of n bodies
-struct cosmos * get_cosmos( int n );
-
-//I couldn't get the math min to work, so I used this
-int min( int x, int y );
 
 //calculates the force interactions between two points, updating both of them
 void point_update_compute(struct body * b1, struct body * b2);
@@ -56,36 +20,6 @@ void point_update_compute(struct body * b1, struct body * b2);
 //once a point has been updated with all other points, it then has it's
 //movement calculated based on the sum of all forces
 void calc_movement(struct cosmos * c, struct body * b);
-
-//gets the next good line, checking for EOF and ignoring whitespace and commented lines
-int getnextgoodline( char * s, int n, FILE * f);
-
-//splits a string into chunks based on the delimiter, resultc is max results
-//returns number of results retrieved
-int chunk_string(char * s, char * delim, int resultc, char ** res);
-
-//frees chunked arrays
-void free_chunks( char ** c , int num);
-
-//code to read a particular body from a file. 
-void read_body( struct body * b, char * s);
-
-//reads a cosmos in from a filename
-struct cosmos * read_cosmos( const char * filename );
-
-//prints a body
-void print_body( struct body * b);
-
-//prints the given cosmos;
-void print_cosmos( struct cosmos * c );
-
-//converts a string to a floating point number
-float string_to_float( char * s );
-
-//gets the sign of a number
-float sign( float f );
-
-//void calc_gravity (struct cosmos * c, struct body * b);
 
 //one iteration of the n_body
 void simple_n_body_iter ( struct cosmos * c );
