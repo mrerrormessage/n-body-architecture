9eda98d709b2a82cadb6526db20ad4a6142b2139
Robert Grider <mrerrormessage@gmail.com>
2009-12-04 18:22:34 -0600

    updated serial code

--- serial/serial.c
+++ serial/serial.c
@@ -2,40 +2,6 @@
 
 
 
-
-void point_update_compute(struct body * b1, struct body * b2){
-
-  float xdiff = b1->x_posn - b2->x_posn;
-  float ydiff = b1->y_posn - b2->y_posn;
-  float dist_sq = pow( xdiff, 2.0) + pow( ydiff, 2.0);
-  float force = GRAV_CONST * b1->mass * b2->mass / dist_sq;
-  float theta = atan(ydiff/xdiff);
-  b1->x_force += force * sign(xdiff) * cos(theta);
-  b1->y_force += force * sign(ydiff) * sin(theta);
-  //may want these lines to be "+=" we'll find out
-  b2->x_force -= force * sign(xdiff) * cos(theta);
-  b2->y_force -= force * sign(ydiff) * sin(theta);
-
-  return;
-}
-
-
-void calc_movement(struct cosmos * c, struct body * b){
-
-  //velocity = acceleration * time
-  //v = v_0 + a * t
-  b->x_velocity += (b->x_force / b->mass) * c->time_step;
-  b->y_velocity += (b->y_force / b->mass) * c->time_step;
-
-  //may need to have this divided by time_step, not sure
-  //x = x_0 + v * t
-  //the '-' here is a hack because we seem to be getting the wrong numbers
-  b->x_posn = b->x_posn - (b->x_velocity * c->time_step);
-  b->y_posn = b->y_posn - (b->y_velocity * c->time_step);
-
-  return;
-}
-
 //one iteration of the n_body
 void simple_n_body_iter ( struct cosmos * c ){
 
@@ -46,14 +12,12 @@ void simple_n_body_iter ( struct cosmos * c ){
       struct body * b2;
       b2 = get_body( c, j);
       
-      point_update_compute(b, b2);
+      point_update_compute(b, b2, GRAV_CONSTANT);
       
     }
-
     //calc movement down here for each body that has had all interactions calculated on it
-    calc_movement(c, b);
+    calc_movement(b, c->time_step);
     
-
   }
   
   return;
--- serial/serial.h
+++ serial/serial.h
@@ -6,20 +6,13 @@
 #include <string.h>
 #include "../lib/cosmos.h"
 
-#define GRAV_CONST 1.000
 #define TIME_STEP 0.5
 //#define NUM_BODIES 50
 //#define WORLD_X_SIZE 500.0
 //#define WORLD_Y_SIZE 500.0
-#define GRAV_CONSTANT 6.67428e-11
+//#define GRAV_CONSTANT 6.67428e-11
+#define GRAV_CONSTANT 1.000
 
-
-//calculates the force interactions between two points, updating both of them
-void point_update_compute(struct body * b1, struct body * b2);
-
-//once a point has been updated with all other points, it then has it's
-//movement calculated based on the sum of all forces
-void calc_movement(struct cosmos * c, struct body * b);
 
 //one iteration of the n_body
 void simple_n_body_iter ( struct cosmos * c );
